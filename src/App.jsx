c1ad73a54ad382a78ae1af937bd19b4bb7c9c29a
ShakirMahmud <shakirmahmud50@gmail.com>
2024-10-16 00:01:04 +0600

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+/**
+|--------------------------------------------------
+| @ i_Shakir
+|--------------------------------------------------
+*/
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,15 +13,22 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Device name='Laptop' price='27000'></Device>
+      <Device name='Mobile' price='14000'></Device>
+      <Device name='Watch' price='2700'></Device>
       <Person></Person>
+      <Student grade='7' score='99'></Student>
+      <Student grade={12} score='85'></Student>
+      <Student grade='9' score='89'></Student>
       <Student></Student>
+      <Developer></Developer>
     </>
   )
 }
 
 function Person(){
-  const age = 25;
-  const money = 20;
+  const age = 15;
+  const money = 12;
   const person = {name:'Shakir', age:12};
   return (
     <div>
@@ -24,15 +37,37 @@ function Person(){
   )
 }
 
-function Student() {
+function Student({grade=1, score=0}) {
   return (
-    <div>
+    <div className='student'>
       <h2>Student</h2>
+      <p>Class: {grade}</p>
+      <p>Score: {score}</p>
     </div>
   )
 }
 
+function Device(props) {
+  return (
+    <div>
+      <h2>This Device: {props.name} and it's Price: {props.price}</h2>
+    </div>
+  )
+}
 
-
+function Developer() {
+  const developerStyle = {
+    margin: '20px',
+    padding:'20px',
+    border:'2px solid black',
+    borderRadius: '16px'
+  }
+  return (
+    <div style={developerStyle}>
+      <h5>Developer</h5>
+      <p>Coding.......</p>
+    </div>
+  )
+}
 
 export default App
